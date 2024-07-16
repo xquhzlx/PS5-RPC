244816ed01210cd6579de2145a7aff0ed3e74f06
хкухзлкс <166228992+xquhzlx@users.noreply.github.com>
2024-07-16 07:12:28 -0500

    🍡🍭
    For those who don't know how to use the "platform" property in the RPC object.

---
+++ index.js
@@ -0,0 +1,16 @@
+const discord = require("discord.js-selfbot-v13");
+const client = new discord.Client();
+
+client.on("ready", async () => {
+    client.user.setPresence({
+        activities: [{
+            name: 'ex',
+            type: 'PLAYING',
+            platform: "ps5",
+            timestamps: { start: Date.now() - (404 * 24 * 60 * 60 * 1000) },
+            assets: { large_image: null, large_text: null, small_image: null, small_text: null }
+        }]
+    });
+});
+
+client.login("XXXXXXXXXXXXXX");
